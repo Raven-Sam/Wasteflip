fc17b936de661c53cb97c44389ea9cb4716f6083
Abiola Zeenat Abdulsalam <81884414+Abiola-Zeenat@users.noreply.github.com>
2024-10-30 23:30:09 +0100

    re setting

---
+++ wasteflips/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import LandingPage from "./pages/Landingpage";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <LandingPage />
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default App;
