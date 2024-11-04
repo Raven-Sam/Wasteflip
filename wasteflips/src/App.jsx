3576f7872909db4c97f0f57ace33c44e01e59d0a
Samuel Akachukwu <akachukwusam@gmail.com>
2024-11-04 08:05:56 +0000

    dashboard--new

--- wasteflips/src/App.jsx
+++ wasteflips/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -5,16 +6,21 @@ import "./App.css";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import LandingPage from "./pages/Landingpage";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/Home" element={<Home />} /> 
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
+
 
 export default App;
